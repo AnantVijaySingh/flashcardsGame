0a5813dcf594df6f4ee96abac330d662f01e6781
anantvijay.23@gmail.com <8Gerrard>
2018-07-12 00:34:37 +0100

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <DeckList/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/DeckList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+class DeckList extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Deck List
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default DeckList;
