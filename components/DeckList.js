f71a96725f216f5a870bec15619041937b259dec
anantvijay.23@gmail.com <anantvijay.23@gmail.com>
2018-07-14 20:21:57 +0100

    Using FlatList to create the decks list view. Used Object.values to convert Redux object to Array as FlatList only accepts an Array for it data prop.
    --- Alternative solution to try ---
    Restructure Redux to Array format, so something like
    
    function myReducer(state=[], action){
    
     switch(action.type){
    
       case GET_ALL_DECKS:
    
           return [...state, action.decks];
    
     }
    
    }

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, FlatList, Platform} from 'react-native';
 import {connect} from 'react-redux';
 import {handleInitialData} from "../actions/shared";
 
+function DeckView({title, questions}) {
+    return (
+        <View style={styles.deck}>
+            <Text style={styles.heading}>{title}</Text>
+            <Text>{questions.length} Cards</Text>
+        </View>
+    )
+}
+
 class DeckList extends React.Component {
+
+    renderItem = ({item}) => {
+        return ( <DeckView { ...item}/>)
+    };
 
     componentDidMount() {
         this.props.dispatch(handleInitialData());
@@ -14,22 +27,45 @@ class DeckList extends React.Component {
         const {decks} = this.props;
 
         return (
-            <View>
-                {
-                    Object.keys(decks).map((deck) =>  (
-                        <View key={decks[deck].title}>
-                            <Text>{decks[deck].title}</Text>
-                        </View>
-                        )
-                    )
-                }
+            <View style={styles.container}>
+                <FlatList
+                    data={Object.values(decks)}
+                    renderItem={this.renderItem}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#F8F8FF',
+        alignSelf: 'stretch'
+    },
+    deck: {
+        backgroundColor: '#FFFFFF',
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: 150,
+        marginTop: 25,
+        marginLeft: 15,
+        marginRight: 15,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding:20,
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.8)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight:'bold',
+    }
 });
 
 function  mapStateToProps(state) {
